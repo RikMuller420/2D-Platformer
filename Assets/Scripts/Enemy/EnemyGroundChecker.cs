65261a997e67b46e1ddd9ec673bac4f4baab4152
Muller Rik <rikmuller420@gmail.com>
2025-04-01 10:07:02 +0300

    Реализовано движение врагов (патруль)

---
+++ Assets/Scripts/Enemy/EnemyGroundChecker.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyGroundChecker : MonoBehaviour
+{
+    [SerializeField] private float _checkBottomDistance = 0.5f;
+    [SerializeField] private float _checkSideDistance = 0.6f;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private Transform _groundCheckPosition;
+
+    public bool IsAbleToMoveForward(Direction moveDirection)
+    {
+        Vector2 rayOrigin = (Vector2)_groundCheckPosition.position + (Vector2.right * (int)moveDirection * _checkSideDistance);
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, _checkBottomDistance, _groundLayer);
+
+        return hit.collider != null;
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyMover.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(EnemyGroundChecker))]
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1.5f;
+    [SerializeField] private float _maxPatrolDistance = 5f;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private EnemyGroundChecker _groundChecker;
+
+    private Direction _moveDirection = Direction.Right;
+    private Vector2 _startPatrolPoint;
+
+    private void Awake()
+    {
+        _startPatrolPoint = transform.position;
+    }
+
+    private void Update()
+    {
+        if (IsDirectionSwapRequired())
+        {
+            SwapMoveDirection();
+        }
+
+        float velocityX = (int)_moveDirection * _speed;
+        _rigidbody.linearVelocityX = velocityX;
+    }
+
+    private bool IsDirectionSwapRequired()
+    {
+        if (IsOutOfPatrolRange())
+        {
+            if (IsFacingToStartPatrolPoint() == false)
+            {
+               return true;
+            }
+        }
+
+        bool canMoveForward = _groundChecker.IsAbleToMoveForward(_moveDirection);
+
+        if (canMoveForward == false)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void SwapMoveDirection()
+    {
+        if (_moveDirection == Direction.Right)
+        {
+            _moveDirection = Direction.Left;
+        }
+        else
+        {
+            _moveDirection = Direction.Right;
+        }
+    }
+
+    private bool IsOutOfPatrolRange()
+    {
+        return ((Vector2)transform.position - _startPatrolPoint).magnitude > _maxPatrolDistance;
+    }
+
+    private bool IsFacingToStartPatrolPoint()
+    {
+        if (transform.position.x > _startPatrolPoint.x)
+        {
+            return _moveDirection == Direction.Left;
+        }
+        else
+        {
+            return _moveDirection == Direction.Right;
+        }
+    }
+}
